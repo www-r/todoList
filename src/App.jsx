663fc28708efbb7a526f9f84e2a7f7c5dcacb9f1
youngeun <jessicak0828@gmail.com>
2023-03-09 21:34:23 +0900

    feat: App.jsx 추가 Title 컴포넌트는 삭제했습니다

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { HeaderDate, Header, Container } from '@/components/index.js';
+
+function App() {
+	return (
+		<div className='App'>
+			<HeaderDate />
+			<Header />
+			<Container />
+		</div>
+	);
+}
+
+export default App;
