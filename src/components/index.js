555d49e374f46ec2c68a4d5c965d6b54e4246c53
youngeun <jessicak0828@gmail.com>
2023-03-09 21:32:28 +0900

    feat: index.js추가 export,impport 하는 컴포넌트들 한번에 관리하는 파일

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Container from '@/components/Container';
+import Header from '@/components/Header';
+import HeaderDate from '@/components/HeaderDate';
+import TodoInput from '@/components/TodoInput';
+import TodoList from '@/components/TodoList';
+// import { getTodo, addTodo, deleteTodo, editTodo } from './api';
+export { Container, Header, TodoInput, TodoList, HeaderDate };
+// export { getTodo, addTodo, deleteTodo, editTodo };
