2d04fddda4b41bcaee25d88ce35554e1085ebdfc
youngeun <jessicak0828@gmail.com>
2023-03-14 00:10:19 +0900

    feat: 투두리스트 수정, 완료여부 기능 추가

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { editTodo, deleteTodo, getTodos } from '@/components/api.js';
+import * as S from './style.js';
 
 const TodoList = ({
 	id,
@@ -11,22 +12,28 @@ const TodoList = ({
 	todos,
 	setTodos
 }) => {
-	console.log(todos);
+	// [done, setDone] = useState(false);
+	const [show, setShow] = useState(true);
+	const [inputValue, setInputValue] = useState(title);
 
-	const editTodoHandler = async e => {
-		console.log(newTodo);
-		if (e.target.closest('.list--text').hasAttribute('contenteditable')) {
-			e.target.closest('list--text').remove('contenteditable');
-			//edit API 사용
-			await editTodo(id, newTodo, done);
-			alert('수정되었습니다!');
-		} else {
-			e.target.closest('.list-text').setAttribute('contenteditable');
-		}
+	const editTodoHandler = async (id, done) => {
+		setShow(!show);
+		console.log(inputValue);
+		// title = editTodo(id, title, done);
+		editTodo(id, inputValue, done);
+
+		// todos.map((todo)=> ?:)
+		const newTodos = todos.map(todo =>
+			todo.id === id
+				? { id, title: inputValue, done, createdAt, updatedAt }
+				: todo
+		);
+
+		setTodos(newTodos);
 	};
 
 	const deleteTodoHandler = async id => {
-		// await deleteTodo(id);
+		await deleteTodo(id);
 		const newTodos = todos.filter(todo => todo.id !== id);
 		setTodos(newTodos);
 	};
@@ -36,21 +43,56 @@ const TodoList = ({
 		todoArr.map(todo => <todoList />);
 	};
 
+	const todoDoneHandler = (id, title, done) => {
+		editTodo(id, title, done);
+		const newTodos = todos.map(todo =>
+			todo.id === id ? { id, title, done: !done, createdAt, updatedAt } : todo
+		);
+		setTodos(newTodos);
+
+		// setArticles(
+		// 	articles.map(article => (article.id === id ? newArticle : article))
+		// );
+	};
+
+	const onChangeHandler = e => {
+		setInputValue(e.target.value);
+	};
+
 	return (
-		<div className='todoList'>
+		<S.TodoList className='TodoList'>
 			<li className='todolistList'>
 				<div className='list--item'>
-					<span className='material-symbols-outlined icon--checkbox true'>
-						check_box_outline_blank
-					</span>
+					<div className='checkbox'>
+						{!done ? (
+							<img
+								className='checkboxImage empty'
+								src='./src/assets/checkbox-empty.png'
+								alt='checkbox empty'
+								onClick={() => {
+									todoDoneHandler(id, title, done);
+								}}
+							/>
+						) : (
+							<img
+								className='checkboxImage checked'
+								src='./src/assets/checkbox-checked.png'
+								alt='checkbox checked'
+								onClick={() => {
+									todoDoneHandler(id, title, done);
+								}}
+							/>
+						)}
+					</div>
 				</div>
-				<div
-					className='list--item list--text'
-					contentEditable
-					suppressContentEditableWarning
-				>
-					{title}
-				</div>
+				{show && (
+					<div className='list--item list--text' suppressContentEditableWarning>
+						{title}
+					</div>
+				)}
+				{!show && (
+					<input type='text' value={inputValue} onChange={onChangeHandler} />
+				)}
 				<div className='list--item list--dates-container'>
 					<p className='list--date'>작성:{createdAt.slice(0, 10)}</p>
 					<p className='list--date'>수정:{updatedAt.slice(0, 10)}</p>
@@ -58,21 +100,21 @@ const TodoList = ({
 				<button className='edit-btn'>
 					<img
 						className='list--item icon icon--edit'
-						src='@/assets/pencil.png'
+						src='./src/assets/pencil.png'
 						alt='edit button'
-						onChange={editTodoHandler}
+						onClick={e => editTodoHandler(id, done)}
 					/>
 				</button>
 				<button className='delete-btn'>
 					<img
 						className='list--item icon icon--delete'
-						src='@/assets/garbage.png'
+						src='./src/assets/garbage.png'
 						alt='delete button'
 						onClick={() => deleteTodoHandler(id)}
 					/>
 				</button>
 			</li>
-		</div>
+		</S.TodoList>
 	);
 };
 
