f92999d24e15bd22459345647a343ff2d21147a3
youngeun <jessicak0828@gmail.com>
2023-03-14 00:10:57 +0900

    feat: 투두리스트 입력란 기능 추가

--- src/components/TodoInput/index.jsx
+++ src/components/TodoInput/index.jsx
@@ -1,42 +1,44 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { addTodo } from '@/components/api.js';
-
+import * as S from './style.js';
 const TodoInput = ({ todos, setTodos }) => {
+	console.log('todos', todos);
 	const inputRef = useRef();
-	const [newtext, setNewtext] = useState('');
-	// const newTodo = setNewtext(newtext);
+	const [newText, setNewText] = useState('');
+
 	const updateTodo = async () => {
 		await addTodo(newTodo);
 	};
-	const submitHandler = async () => {
-		setNewtext(inputRef?.current.value);
-		// out API 함수 사용
-		const newInputTodo = await addTodo(newtext);
+	const submitHandler = async e => {
+		e.preventDefault();
+		// setNewtext(inputRef?.current.value);
+		console.log('newText', newText);
+		// put API 함수 사용
+		const newInputTodo = await addTodo(newText);
+		console.log('newInputTodo', newInputTodo);
+		setTodos(todos => [newInputTodo, ...todos]);
+	};
 
-		console.log('new', newInputTodo);
-		console.log('after set: ', newtext);
+	const onChangeHandler = e => {
+		setNewText(e.target.value);
 	};
 	const keyDownHandler = e => {
 		e.key === 'Enter'
 			? async () => {
 					await submitHandler();
-					renderTodoHandler();
 			  }
 			: function () {
 					return;
 			  };
 	};
-	useEffect(() => {
-		if (!newtext) return;
-		console.log('after useEffect', newtext);
-		submitHandler();
-	}, [newtext]);
 
 	return (
-		<div className='TodoInput'>
-			<input ref={inputRef} type='text' onKeyDown={keyDownHandler} />
-			<button onClick={submitHandler}>추가</button>
-		</div>
+		<S.TodoInput className='TodoInput'>
+			<form onSubmit={submitHandler}>
+				<input onChange={onChangeHandler} type='text' />
+				<button type='submit'>추가</button>
+			</form>
+		</S.TodoInput>
 	);
 };
 
